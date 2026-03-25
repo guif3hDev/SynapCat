a042a4c42d2524c27a28e1939a190612bfec3905
Guilherme Felipe <guilhermef.lima169@gmail.com>
2026-03-25 18:08:24 -0300

    SynapCat V1.2
    Sistema de cargo automático, com mensagens personalizadas.

--- main.py
+++ main.py
@@ -12,20 +12,34 @@ async def on_ready():
 async def on_message(msg:discord.message):
     if msg.author.bot:
         return
-    await msg.reply(f"O usuário {msg.author.mention} enviou uma mensagem!")
+    await msg.reply(f"Miaauu! O usuário {msg.author.mention} enviou uma mensagem!")
+    await bot.process_commands(msg)
 
 @bot.event
 async def on_member_join(membro:discord.Member):
     canal = bot.get_channel(1485411749980012574)
-    await canal.send(f"{membro.mention} entrou no servidor!")
+    await canal.send(f"Miaaau... {membro.mention} entrou no servidor!")
+
+    role_id = 1485975376978247811
+    role = membro.guild.get_role(role_id)
+    if role is None:
+        print("cargo não encontrado")
+        return
+    
+    try:
+        await membro.add_roles(role)
+        print(f"Miaaau! Cargo adicionado para {membro.name}")
+    except discord.Forbidden:
+        print("Miauu... Não tenho permissão para adicionar cargos.")
+    except Exception as e:
+        print(f"erro: {e}")
 
 @bot.event
 async def on_member_remove(membro):
     canal = bot.get_channel(1485416699615514726)
-    await canal.send(f"{membro.mention} saiu do servidor!")
+    await canal.send(f"Miaauu! {membro.mention} saiu do servidor!")
 
 @bot.command()
 async def ola(ctx: commands.Context):
     nome = ctx.author.name
-    await ctx.reply(f"Olá, {nome}! Tudo bem?")
-
+    await ctx.reply(f"Miaauu! Olá, {nome}! Tudo bem?")
